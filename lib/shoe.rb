826f59f751bf55f5aedcf7a6fa2bc85e79ff2199
Tyler Masterson <tylercmasterson@gmail.com>
2019-07-17 22:14:53 -0600

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,11 +8,7 @@ class Shoe
     @brand = brand
     BRANDS << brand unless BRANDS.include?(brand)
   end
-  # def brand= brand
-  #   @brand = brand
-  #   BRANDS << brand
-  # end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
